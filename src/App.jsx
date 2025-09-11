743daa5662ac24a1c5dd1b7b74ac81ef67e0c456
Jose <jomaamga@outlook.es>
2025-09-11 23:08:44 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+const App = () => {
+  return (
+    <>
+      <h1>Hola!</h1>
+      <a href="https://gallardo.dev">Abre este link</a>
+    </>
+  )
+}
+
+export default App
